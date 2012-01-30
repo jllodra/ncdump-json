6a3cfa8154019cf55aa6d846a874e4add8aa9134
Josep Llodrà <jllodra@socib.es>
2012-01-30 12:02:25 +0100

    Cmake'd

---
+++ CMakeFiles/ncdump-json.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ncdump-json.dir/ncdump.c.o"
+  "CMakeFiles/ncdump-json.dir/vardata.c.o"
+  "CMakeFiles/ncdump-json.dir/dumplib.c.o"
+  "CMakeFiles/ncdump-json.dir/indent.c.o"
+  "CMakeFiles/ncdump-json.dir/nctime.c.o"
+  "ncdump-json.pdb"
+  "ncdump-json"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/ncdump-json.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
